9377f6571cecd0a5ed9d5e1afc5ae1c027733832
ge94zag <marcel.munaba@tum.de>
2023-09-07 01:52:05 +0700

    sidebar updates, javascript bug fixes

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,8 @@
 function courses() {
   var x = document.getElementById("tum-courses");
-  if (x.style.display === "none") {
-    x.style.display = "block";
-  } else {
+  if (x.style.display === "block") {
     x.style.display = "none";
+  } else {
+    x.style.display = "block";
   }
 }
-
-function hidebutton() {
-    var x = document.getElementById("sidebar-id");
-    if (x.style.display === "none") {
-      x.style.display = "block";
-    } else {
-      x.style.display = "none";
-    }
-  }
