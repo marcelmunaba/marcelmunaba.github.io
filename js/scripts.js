26b50a2628b56823832e012898a2717c194dafe2
ge94zag <marcel.munaba@tum.de>
2023-09-07 01:35:24 +0700

    more updates, added a bit of javascript

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+function courses() {
+  var x = document.getElementById("tum-courses");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function hidebutton() {
+    var x = document.getElementById("sidebar-id");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
