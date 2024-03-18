07865765018b0d0cc5e2d26aed3433c61d32ef88
Marcel <marcel.munaba@tum.de>
2024-03-18 18:33:11 +0100

    added mori, censorbot, recolor, favicon, and cv download

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
+// Show Courses
 function courses() {
   var x = document.getElementById("tum-courses");
   if (x.style.display === "block") {
@@ -7,9 +8,51 @@ function courses() {
   }
 }
 
-// function email() {
-//   var x = document.getElementByClass("email-icon");
-//   if (x.matches(':hover')) {
+// Image Slideshow
+document.addEventListener('DOMContentLoaded', function() { 
+  //addEventListener ensures that the DOM is loaded first
+  let slideIndex = 1;
+  showSlides(slideIndex);
 
-//   }
-// }
+  function plusSlides(n) {
+      showSlides(slideIndex += n);
+  }
+
+  function currentSlide(n) {
+      showSlides(slideIndex = n);
+  }
+
+  function showSlides(n) {
+      let i;
+      let slides = document.getElementsByClassName("mySlides");
+      let dots = document.getElementsByClassName("dot");
+      if (n > slides.length) {slideIndex = 1}    
+      if (n < 1) {slideIndex = slides.length}
+      for (i = 0; i < slides.length; i++) {
+          slides[i].style.display = "none";  
+      }
+      for (i = 0; i < dots.length; i++) {
+          dots[i].className = dots[i].className.replace(" active", "");
+      }
+      slides[slideIndex-1].style.display = "block";  
+      dots[slideIndex-1].className += " active";
+  }
+
+  // Event listeners for next and previous buttons, so that it can be accessed after the first execution
+  document.querySelector('.prev').addEventListener('click', function() {
+      plusSlides(-1);
+  });
+
+  document.querySelector('.next').addEventListener('click', function() {
+      plusSlides(1);
+  });
+
+  // Dots navigation
+  let dotElements = document.getElementsByClassName("dot");
+  for (let i = 0; i < dotElements.length; i++) {
+      dotElements[i].addEventListener('click', function() {
+          currentSlide(i + 1);
+      });
+  }
+  
+});
