7689b7757c267dddaeedc366ee5e5e8f4c344dae
Marcel <marcel.munaba@tum.de>
2024-03-27 20:15:38 +0100

    revert "show courses taken" button to old collapse method using js

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,9 @@
 // Show Courses
-// function courses() {
-//     var x = document.getElementById("tum-courses");
-//     x.classList.toggle("fade-in");
-//     if (x.classList.contains("fade-in")) {
-//         x.style.display = "block"; // Ensure the element is displayed before fading in
-//         setTimeout(function() {
-//             x.style.opacity = 1; // Set opacity to fully visible after a short delay
-//         }, 10);
-//     } else {
-//         x.style.opacity = 0; // Set opacity to 0 when fading out
-//         setTimeout(function() {
-//             x.style.display = "none"; // Hide the element after the fading animation
-//         }, 500); // Adjust the delay to match the transition duration
-//     }
-// }
+function courses() {
+    var x = document.getElementById("tum-courses");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
